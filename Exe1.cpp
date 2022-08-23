6eaf5d3d75a08ec0d37ed3d1b1e403b45c37da14
huynhthuan <huynhthuanvugia@gmail.com>
2022-08-23 23:12:11 +0700

    Done all exe

--- Exe1.cpp
+++ Exe1.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
+
+using namespace std;
 
 int getSumUnitNumber(int number, int unitStart)
 {
@@ -76,27 +79,41 @@ void strRev(char str[])
 	}
 }
 
+int countElement(int arr[], int element, int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == element)
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
 int main()
 {
 	// Exe 1
 
-	// int number;
+	int number;
 
-	// printf("Nhap so N: ");
-	// scanf_s("%d", &number);
+	printf("Nhap so N: ");
+	scanf_s("%d", &number);
 
-	// if (number >= 20 && number <= 50) {
-	//	printf("%d nam trong khoang 20 -> 50", number);
-	//	return 1;
-	// };
+	if (number >= 20 && number <= 50) {
+		printf("%d nam trong khoang 20 -> 50", number);
+		return 1;
+	};
 
-	// printf("%d khong nam trong khoang 20 -> 50", number);
+	printf("%d khong nam trong khoang 20 -> 50", number);
 
 	//===================================================
 
 	// Exe 2
 
-	/*int number;
+	int number;
 
 	printf("Nhap so N: ");
 	scanf_s("%d", &number);
@@ -110,65 +127,63 @@ int main()
 		}
 	}
 
-	*/
-
 	//===================================================
 
 	// Exe 3
 
-	// int number;
+	int number;
 
-	// printf("Nhap so N: ");
-	// scanf_s("%d", &number);
+	printf("Nhap so N: ");
+	scanf_s("%d", &number);
 
-	// int result = getSumUnitNumber(number, 1);
+	int result = getSumUnitNumber(number, 1);
 
-	// printf("Tong tat ca cac so cua so %d la: %d", number, result);
+	printf("Tong tat ca cac so cua so %d la: %d", number, result);
 
 	//===================================================
 
 	// Exe 4
 
-	// char str[50] = "";
-	// int countNumber = 0, countCharacter = 0;
+	char str[50] = "";
+	int countNumber = 0, countCharacter = 0;
 
-	// printf("Nhap chuoi bat ki: ");
+	printf("Nhap chuoi bat ki: ");
 
-	// fgets(str, sizeof(str), stdin);
+	fgets(str, sizeof(str), stdin);
 
-	// str[strlen(str) - 1] = '\0';
+	str[strlen(str) - 1] = '\0';
 
-	// for (int i = 0; i < strlen(str); i++)
-	// {
-	// 	if (isdigit(str[i]))
-	// 	{
-	// 		countNumber++;
-	// 	}
-	// 	else
-	// 	{
-	// 		countCharacter++;
-	// 	}
-	// }
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (isdigit(str[i]))
+		{
+			countNumber++;
+		}
+		else
+		{
+			countCharacter++;
+		}
+	}
 
-	// printf("So chu cai trong chuoi %s la: %d\n", str, countCharacter);
-	// printf("So chu so trong chuoi %s la: %d\n", str, countNumber);
+	printf("So chu cai trong chuoi %s la: %d\n", str, countCharacter);
+	printf("So chu so trong chuoi %s la: %d\n", str, countNumber);
 
 	//===================================================
 
 	// Exe 5
 
-	/*int number;
+	int number;
 
 	printf("Nhap so N: ");
 	scanf_s("%d", &number);
 	int result = calcFactorial(number);
-	printf("\nGiai thua cua so %d la: %d", number, result);*/
+	printf("\nGiai thua cua so %d la: %d", number, result);
 
 	//===================================================
 
 	// Exe 6
 
-	/*int numberN;
+	int numberN;
 	int numberM;
 	int sum = 0;
 
@@ -185,86 +200,113 @@ int main()
 		}
 	}
 
-	printf("Tong cac so chia het cho 2 va 5, tu %d -> %d la: %d", numberN, numberM, sum);*/
+	printf("Tong cac so chia het cho 2 va 5, tu %d -> %d la: %d", numberN, numberM, sum);
 
 	//===================================================
 
 	// Exe 7
 
-	// int sizeArr;
-	// int numbers[100];
+	int sizeArr;
+	int numbers[100];
 
-	// printf("Nhap so N: ");
-	// scanf("%d", &sizeArr);
+	printf("Nhap so N: ");
+	scanf("%d", &sizeArr);
 
-	// for (int i = 0; i < sizeArr; i++)
-	// {
-	// 	printf("Nhap gia tri phan tu %d : ", i);
-	// 	scanf("%d", &numbers[i]);
-	// }
+	for (int i = 0; i < sizeArr; i++)
+	{
+		printf("Nhap gia tri phan tu %d : ", i);
+		scanf("%d", &numbers[i]);
+	}
 
-	// sortArrNumber(numbers, sizeArr);
+	sortArrNumber(numbers, sizeArr);
 
-	// arrDump(numbers, sizeArr);
+	arrDump(numbers, sizeArr);
 
 	//===================================================
 
 	// Exe 8
 
-	// int sizeArr;
-	// int numbers[100];
-	// int numbersArrAsc[100];
-	// int numbersArrAscSize = 0;
+	int sizeArr;
+	int numbers[100];
+	int numbersArrAsc[100];
+	int numbersArrAscSize = 0;
 
-	// printf("Nhap so N: ");
-	// scanf("%d", &sizeArr);
+	printf("Nhap so N: ");
+	scanf("%d", &sizeArr);
 
-	// for (int i = 0; i < sizeArr; i++)
-	// {
-	// 	printf("Nhap gia tri phan tu %d : ", i);
-	// 	scanf("%d", &numbers[i]);
-	// }
+	for (int i = 0; i < sizeArr; i++)
+	{
+		printf("Nhap gia tri phan tu %d : ", i);
+		scanf("%d", &numbers[i]);
+	}
 
-	// for (int j = 0; j < sizeArr; j++)
-	// {
-	// 	numbersArrAsc[j] = numbers[j];
-	// 	numbersArrAscSize++;
-	// 	if (numbers[j] > numbers[j + 1])
-	// 		break;
-	// }
+	for (int j = 0; j < sizeArr; j++)
+	{
+		numbersArrAsc[j] = numbers[j];
+		numbersArrAscSize++;
+		if (numbers[j] > numbers[j + 1])
+			break;
+	}
 
-	// arrDump(numbersArrAsc, numbersArrAscSize);
+	arrDump(numbersArrAsc, numbersArrAscSize);
 
 	//===================================================
 
 	// Exe 9
 
-	// char fullname[50] = "";
+	char fullname[50] = "";
 
-	// printf("Nhap ho va ten cua ban: ");
-	// fgets(fullname, sizeof(fullname), stdin);
+	printf("Nhap ho va ten cua ban: ");
+	fgets(fullname, sizeof(fullname), stdin);
 
-	// fullname[strlen(fullname) - 1] = '\0';
+	fullname[strlen(fullname) - 1] = '\0';
 
-	// char *token = strtok(fullname, " ");
+	char *token = strtok(fullname, " ");
 
-	// printf("Ho va ten dao nguoc la: ");
+	printf("Ho va ten dao nguoc la: ");
 
-	// while (token != NULL)
-	// {
-	// 	strRev(token);
-	// 	printf("%s ", token);
-	// 	token = strtok(NULL, " ");
-	// }
+	while (token != NULL)
+	{
+		strRev(token);
+		printf("%s ", token);
+		token = strtok(NULL, " ");
+	}
 
 	//===================================================
 
 	// Exe 10
 
-	// char fullname[50] = "";
+	char fullname[300];
+	char target;
 
-	// printf("Nhap ho va ten cua ban: ");
-	// fgets(fullname, sizeof(fullname), stdin);
+	int hasCheckDuplicate[300];
+	int indexDuplicate = 0;
 
-	// fullname[strlen(fullname) - 1] = '\0';
+	printf("Nhap ho va ten cua ban: ");
+
+	fgets(fullname, sizeof(fullname), stdin);
+
+	fullname[strlen(fullname) - 1] = '\0';
+
+	for (int i = 0; i < strlen(fullname); i++)
+	{
+		int count = 1;
+
+		if (isspace(fullname[i]) || countElement(hasCheckDuplicate, i, indexDuplicate) != 0)
+		{
+			continue;
+		}
+
+		for (int j = i + 1; j < strlen(fullname); j++)
+		{
+			if (fullname[i] == fullname[j])
+			{
+				count++;
+				hasCheckDuplicate[indexDuplicate] = j;
+				indexDuplicate++;
+			}
+		}
+
+		printf("Ki tu %c xuat hien %d lan\n", fullname[i], count);
+	}
 }
